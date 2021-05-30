82a6aef194e61a0a8a18cd4d5479ed444271247b
babaktaremi <babaktaremi@yahoo.com>
2021-05-30 11:53:23 +0430

    Bug Fix

--- GoogleCaptchaComponent/wwwroot/Scripts/JsOfReCAPTCHA.js
+++ GoogleCaptchaComponent/wwwroot/Scripts/JsOfReCAPTCHA.js
@@ -1,9 +1,15 @@
 ﻿function render_recaptcha(dotNetObj, selector, sitekey) {
-    return grecaptcha.render(selector, {
-        'sitekey': sitekey,
-        'callback': (response) => { dotNetObj.invokeMethodAsync('CallbackOnSuccess', response); },
-        'expired-callback': () => { dotNetObj.invokeMethodAsync('CallbackOnExpired'); }
-    });
+    setTimeout(function () {
+        if (typeof grecaptcha === 'undefined' || typeof grecaptcha.render === 'undefined') {
+            this.render_recaptcha();
+        } else {
+            return grecaptcha.render(selector, {
+                'sitekey': sitekey,
+                'callback': (response) => { dotNetObj.invokeMethodAsync('CallbackOnSuccess', response); },
+                'expired-callback': () => { dotNetObj.invokeMethodAsync('CallbackOnExpired'); }
+            });
+        }
+    }.bind(this), 100);
 };
 
 function getResponse(widgetId) {
